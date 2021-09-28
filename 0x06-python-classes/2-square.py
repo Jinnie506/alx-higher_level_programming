0554dcdbb540ad898180daeac14ac842bebc0da1
Ifeoma <ojiniifeoma@gmail.com>
2021-09-28 08:02:09 -0700

    fixed error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""defines a class Square"""
 class Square:
         """Represents a square
         Attributes:
         __size (int): size of a side of the square
-    """
+        """
         def __init__(self, size=0):
             """initializes the square
             Args:
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,47 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
 
 class Square:
-        '''Initialize Class Square
-        '''
+        """Represents a square
+        Attributes:
+        __size (int): size of a side of the square
+        """
         def __init__(self, size=0):
-            self.size = size
+                """initializes the square
+                Args:
+                size (int): size of a side of the square
+                Returns:
+                None
+                """
+                self.size = size
+        def area(self):
+                """calculates the square's area
+                   Returns:
+                   The area of the square
+                """
+                return (self.__size) ** 2
 
         @property
         def size(self):
-            return self.__size
+                """getter of __size
+                  Returns:
+                  The size of the square
+                """
+                return self.__size
 
         @size.setter
-        def size(self, size):
-            if type(size) != int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        def area(self):
-            return (self.size * self.size)
+        def size(self, value):
+                """setter of __size
+                Args:
+                value (int): the size of a size of the square
+                Returns:
+                None
+                """
+                if type(value) is not int:
+                        raise TypeError("size must be an integer")
+                else:
+                        if value < 0:
+                                raise ValueError("size must be >= 0")
+                        else:
+                                self.__size = value
