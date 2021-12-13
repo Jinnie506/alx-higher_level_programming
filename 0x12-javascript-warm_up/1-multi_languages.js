ecd9919a295054c160a0851fcfd24fa82f7e0faa
Jinnie506 <ojiniifeoma@gmail.com>
2021-12-13 19:59:47 +0000

    fix err

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 /* prints statements about c, python, and javascript */
 const Statement1 = 'C is fun';
 const Statement2 = 'Python is cool';
-const Statement3 = 'Javascript is amazing';
+const Statement3 = 'JavaScript is amazing';
 console.log(Statement1);
 console.log(Statement2);
 console.log(Statement3);
