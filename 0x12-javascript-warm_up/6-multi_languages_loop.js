817c946e51e8c4a5c2d31127fcc85ca2912f1284
Jinnie506 <ojiniifeoma@gmail.com>
2021-12-13 19:56:25 +0000

    completed task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-/*
-   script that prints 3 lines but by using an array of string and a loop
-*/
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let line;
-for (line of args) {
-  console.log(line);
+'use strict';
+let args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let arg in args) {
+  console.log(args[arg]);
 }
+
